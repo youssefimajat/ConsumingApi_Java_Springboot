03079b6e31ac46272807b4fc6ea7d96a18114cea
youssefimajat <youimajat@gmail.com>
2023-10-13 12:32:02 -0500

    Producer

---
+++ src/main/java/practice/consumingrestapi/ConsumingRestApiApplication.java
@@ -0,0 +1,13 @@
+package practice.consumingrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumingRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumingRestApiApplication.class, args);
+    }
+
+}
---
+++ src/main/java/practice/consumingrestapi/bean/Student.java
@@ -0,0 +1,12 @@
+package practice.consumingrestapi.bean;
+
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Student {
+    private int idStudent;
+    private String nameStudent;
+    private String emailStudent;
+}
