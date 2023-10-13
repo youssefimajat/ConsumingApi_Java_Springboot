cc7df94a70a8b880b14e3a3fb7bd2ad93528bfa3
youssefimajat <95992103+youssefimajat@users.noreply.github.com>
2023-10-13 12:35:59 -0500

    Producer

---
+++ ConsumingRestApi/src/test/java/practice/consumingrestapi/ConsumingRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package practice.consumingrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumingRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
